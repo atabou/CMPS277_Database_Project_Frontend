8ceea3d902bbeb769177ee6a471da71446e30e2c
florardu <andretabourian@hotmail.com>
2021-05-02 17:11:21 +0300

    Updated Company functionality for the website

--- components/SideBar.js
+++ components/SideBar.js
@@ -14,12 +14,12 @@ class SideBarComponent extends HTMLElement {
                         <li class="nav-item">
                             <a class="nav-link active" href="#">
                                 <span data-feather="home"></span>
-                                Dashboard <span class="sr-only">(current)</span>
+                                Dashboard
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="javascript:loadCompany();">
+                            <a id="CompanySection" class="nav-link" href="javascript:loadCompany();">
                                 <span data-feather="globe"></span>
                                 Companies
                             </a>
@@ -115,8 +115,120 @@ const endpoint = "http://127.0.0.1:5000/api"
 
 function loadCompany() {
 
-    table = document.getElementsByTagName("table-component");
+    // Deactivate current section
+    const currentActiveSection = document.getElementsByClassName("nav-link active");
+    currentActiveSection[0].setAttribute("class", "nav-link");
+
+    // Set section to Companies
+    const currentTable = document.getElementById("CompanySection");
+    currentTable.setAttribute("class", "nav-link active");
+
+    // Set title of the page to Company
+    document.getElementsByTagName("pageheader-component")[0].setAttribute("page-title", "Company");
+
+    // Set endpoint of table to /company
+    const table = document.getElementsByTagName("table-component");
     table[0].setAttribute("endpoint", endpoint + "/company");
+
+    // Construct modal content to create a new company
+    const createModal = /*html*/`
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Company</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <form>
+                <div class="form-group">
+                    <label for="CompanyTableName">Company Name *</label>
+                    <input type="text" class="form-control" id="CompanyTableName" aria-describedby="Enter the compnay name" placeholder="Company Name">
+                </div>
+                <div class="form-group">
+                    <label for="CompanyTableLocation">Company Location *</label>
+                    <input type="text" class="form-control" id="CompanyTableLocation" aria-describedby="Enter the company location" placeholder="Company Location">
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary submit-record">Submit</button>
+            <button type="button" class="btn btn-secondary dismiss-modal" data-dismiss="modal">Close</button>
+        </div>
+    `;
+
+    // Construct modal content to delete a new company (This is just an example, we should not delete companies)
+    const deleteModal = /*html*/`
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Delete Company</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="modal-body">
+            Are you sure that you want to delete this record?
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary submit-delete-record">
+                <span class="sr-only">Yes</span>
+            </button>
+            <button type="button" class="btn btn-secondary dismiss-delete-record" data-dismiss="modal">No</button>
+        </div>
+    `;
+
+    // Update the page header components with new modal content and function for the buttons New Record and Delete Record
+    const page_header = document.getElementsByTagName("pageheader-component");
+    page_header[0].loadFunctions( 
+        createModal, 
+        () => {
+
+            CompanyName = document.getElementById("CompanyTableName").value;
+            CompanyLocation = document.getElementById("CompanyTableLocation").value;
+
+            if( CompanyName === "" && CompanyLocation === "" ) {
+
+                alert("You have not filled the required fields.");
+
+            } else {
+
+                const loadingIcon = document.createElement("span");
+                loadingIcon.setAttribute("class", "spinner-border spinner-border-sm");
+                loadingIcon.setAttribute("role", "status");
+                loadingIcon.setAttribute("aria-hidden", "true");
+
+                const submitButton = document.getElementsByClassName("submit-record");
+                submitButton[0].setAttribute("disabled", true);
+                submitButton[0].appendChild(loadingIcon);
+
+
+                fetch( endpoint + "/company", {
+
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        "Name": CompanyName,
+                        "Location": CompanyLocation
+                    })
+    
+                }).then( (res) => {
+
+                    const submitButton = document.getElementsByClassName("submit-record");
+                    submitButton[0].removeAttribute("disabled");
+                    submitButton[0].removeChild(submitButton[0].lastChild);
+
+                    $('#addNewRecord').modal('hide');
+
+                })
+
+            }
+
+            
+
+        },
+        deleteModal,
+        null
+    );
 
 }
 
--- components/sub-components/PageHeader.js
+++ components/sub-components/PageHeader.js
@@ -2,21 +2,16 @@ class PageHeader extends HTMLElement {
 
     constructor( ) {
 
-        super();        
-
-    }
-
-    connectedCallback() {
-        
+        super();
 
         this.innerHTML = /*html*/`
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">${this.getAttribute("page-title")}</h1>
                 <div class="btn-toolbar mb-2 mb-md-0">
 
-                    <button type="button" class="btn btn-success mr-1" data-toggle="modal" data-target="#addNewRecord">New Record</button>
+                    <button type="button" class="btn btn-success mr-1 create-record" data-toggle="modal" data-target="#addNewRecord">New Record</button>
 
-                    <button type="button" class="btn btn-danger mr-2">Delete Record</button>
+                    <button type="button" class="btn btn-danger mr-2 delete-record" data-toggle="modal" data-target="#deleteRecord">Delete Record</button>
 
                     <div class="btn-group mr-2">
                         <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
@@ -36,29 +31,105 @@ class PageHeader extends HTMLElement {
             <!-- Modal -->
             <div class="modal fade" id="addNewRecord" tabindex="-1" role="dialog" aria-labelledby="addNewRecord" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
+                    <div id="addNewRecordModal" class="modal-content">
+                        
                     </div>
                 </div>
             </div>
 
-
-
+            <!-- Modal -->
+            <div class="modal fade" id="deleteRecord" tabindex="-1" role="dialog" aria-labelledby="deleteRecord" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div id="deleteRecordModal" class="modal-content">
+                        
+                    </div>
+                </div>
+            </div>
         `;
 
+
+    }
+
+    connectedCallback() {
+        
+        // Replace the icons
         feather.replace();
+
+        const add_btn = this.getElementsByClassName("create-record");
+        add_btn[0].setAttribute("disabled", "true");
+
+        const delete_btn = this.getElementsByClassName("delete-record");
+        delete_btn[0].setAttribute("disabled", "true");
+        
+
+    }
+
+    static get observedAttributes() {
+        return [
+            "page-title"
+        ];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+
+        if( name === "page-title" ) {
+            const title = this.getElementsByTagName("h1");
+            title[0].innerHTML = newValue;
+        }
+    
+    }
+
+    loadFunctions( createModal, createFn = null, deleteModal, deleteFn = null ) {
+
+        if ( createFn !== null ) {
+
+            this.createButton = createFn;
+
+            const create_record_button = this.getElementsByClassName("create-record");
+
+            create_record_button[0].removeAttribute("disabled");
+
+            create_record_button[0].addEventListener("click", ((modalContent, fn) => {
+
+                const modal = document.getElementById("addNewRecordModal");
+                modal.innerHTML = modalContent;
+
+                const submitBtn = document.getElementsByClassName("submit-record");
+                submitBtn[0].addEventListener("click", fn);
+
+            })(createModal, this.createButton));
+
+        } else {
+
+            const create_record_button = this.getElementsByClassName("create-record");
+            create_record_button[0].setAttribute("disabled", "true");
+
+        }
+
+        if (deleteFn !== null) {
+
+            this.deleteButton = deleteFn;
+
+            const delete_record_button = this.getElementsByClassName("delete-record");
+
+            delete_record_button[0].removeAttribute("disabled");
+
+            delete_record_button[0].addEventListener("click", ((modalConent, fn) => {
+
+                const modal = document.getElementById("deleteRecordModal");
+                modal.innerHTML = modalConent;
+
+                const deleteBtn = document.getElementsByClassName("submit-delete-record");
+                deleteBtn[0].addEventListener("click", fn);
+
+            })(deleteModal, this.deleteButton));
+
+        } else {
+
+            const delete_record_button = this.getElementsByClassName("delete-record");
+            delete_record_button[0].setAttribute("disabled", "true");
+
+        }
 
     }
 
